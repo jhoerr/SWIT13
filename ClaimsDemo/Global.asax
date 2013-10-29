cc5aff81c0c5f15804f5179c1335fab858ed8bdd
John Hoerr <jhoerr@gmail.com>
2013-10-29 09:35:52 -0400

    base commit

---
+++ ClaimsDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ClaimsDemo.MvcApplication" Language="C#" %>
---
+++ ClaimsDemo/Models/UserInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace ClaimsDemo.Models
+{
+    public class UserInfo
+    {
+        public string Name { get; set; }
+        public List<string> Roles { get; set; }
+    }
+}
